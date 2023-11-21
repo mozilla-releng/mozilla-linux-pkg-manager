bed2e451dce7c84f988e261a9f293143f4aa00f4
Gabriel Bustamante <gbustamante@mozilla.com>
2023-11-21 16:21:38 -0600

    Fix lint

--- taskcluster/scripts/codecov-upload.py
+++ taskcluster/scripts/codecov-upload.py
@@ -17,9 +17,9 @@ def fetch_secret(secret_name):
     return r.json()["secret"]
 
 
-token = fetch_secret(
-    "project/mozilla/mozilla-linux-pkg-manager/level-1/ci"
-)["codecov_api_token"]
+token = fetch_secret("project/mozilla/mozilla-linux-pkg-manager/level-1/ci")[
+    "codecov_api_token"
+]
 uploader = FETCHES_DIR / "codecov"
 uploader.chmod(uploader.stat().st_mode | stat.S_IEXEC)
 subprocess.run(
